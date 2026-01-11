6a1467427262e7ca631c73006d84f2b253036496
jujudusud <jujudusud@gmail.com>
2026-01-12 00:18:32 +0100

    remove main.cc modifs

--- sources/main.cc
+++ sources/main.cc
@@ -15,41 +15,43 @@
 #if defined(__APPLE__)
 #include <mach-o/dyld.h>
 #endif
-// Ajout mutliplateforme
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
-const char* get_locale_path(std::string& buf) {
-#ifdef _WIN32
-    std::wstring wpath(512, L'\0');
-    DWORD len = GetModuleFileNameW(NULL, &wpath[0], wpath.size());
-    if (len == 0 || len == 512) return nullptr;
-    wpath.resize(len);
-    
-    // UTF-16 -> UTF-8
-    int utf8size = WideCharToMultiByte(CP_UTF8, 0, wpath.data(), -1, NULL, 0, NULL, NULL);
-    buf.resize(utf8size);
-    WideCharToMultiByte(CP_UTF8, 0, wpath.data(), -1, &buf[0], utf8size, NULL, NULL);
-#else
-    // Linux/macOS existant
-    buf.resize(512);
-    ssize_t len = readlink("/proc/self/exe", &buf[0], buf.size());
-    if (len <= 0) {
-#ifdef __APPLE__
-        uint32_t size = buf.size();
-        _NSGetExecutablePath(&buf[0], &size);
-        buf.resize(size);
-#endif
-    }
-#endif
-    return buf.c_str();
-}
-// Fin de l'ajout multiplateforme
 
 namespace Icon {
 #include "../resources/application/FreeMajor.xpm"
 };
+
+#if defined(_WIN32)
+static const char *get_locale_path(std::string &buf)
+{
+    buf.resize(PATH_MAX);
+    buf.resize(GetModuleFileNameA(nullptr, &buf[0], buf.size()));
+
+    if (buf.empty())
+        return nullptr;
+
+    size_t pos = buf.rfind('\\');
+    if (pos == buf.npos)
+        return nullptr;
+
+    buf.resize(pos + 1);
+    buf.append("..\\share\\locale\\");
+    return buf.c_str();
+}
+#elif defined(__APPLE__)
+static const char *get_locale_path(std::string &buf)
+{
+    uint32_t size = 0;
+    _NSGetExecutablePath(nullptr, &size);
+    buf.resize(size);
+    _NSGetExecutablePath(&buf[0], &size);
+    for (unsigned i = 0; i < 2; ++i) {
+        while (!buf.empty() && buf.back() != '/') buf.pop_back();
+        while (!buf.empty() && buf.back() == '/') buf.pop_back();
+    }
+    buf.append("/Resources/locale");
+    return buf.c_str();
+}
+#endif
 
 int main(int argc, char *argv[])
 {
